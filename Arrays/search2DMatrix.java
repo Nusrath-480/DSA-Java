061901351377b6f7e0e259b8624240334d7ea36c
Nusrath-480 <nusrathbegum480@gmail.com>
2025-11-05 15:44:05 +0530

    Added Search a 2D Matrix (LeetCode 74) using staircase

--- Arrays/search2DMatrix.java
+++ Arrays/search2DMatrix.java
@@ -1,33 +1,25 @@
 // Link: https://leetcode.com/problems/search-a-2d-matrix/
 // Language: Java
 // Problem: 74. Search a 2D Matrix
-// Topic: Arrays / Binary Search
+// Topic: Arrays
+
 // Date: 5 Nov 2025
 
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
+        int row = 0;
+        int col = matrix[0].length - 1;  // start from top-right corner
 
-        int left = 0;
-        int right = rows * cols - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            // convert mid into row and column index
-            int row = mid / cols;
-            int col = mid % cols;
-
+        while (row < matrix.length && col >= 0) {
             if (matrix[row][col] == target) {
-                return true;
-            } else if (matrix[row][col] < target) {
-                left = mid + 1;
+                return true; // found target
+            } else if (matrix[row][col] > target) {
+                col--; // move left
             } else {
-                right = mid - 1;
+                row++; // move down
             }
         }
 
-        return false;
+        return false; // not found
     }
 }
